719aa9181eeed2b697c38d59de9ead3ad10477c0
aiacob <andreiiacob15@gmail.com>
2023-03-02 14:23:28 +0200

    init service

---
+++ ingredient-service/src/main/java/com/chefscorner/ingredient/IngredientServiceApplication.java
@@ -0,0 +1,11 @@
+package com.chefscorner.ingredient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientServiceApplication.class, args);
+    }
+}
