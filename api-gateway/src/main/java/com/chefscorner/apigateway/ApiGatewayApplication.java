c3a841414bb0cf9673902042286c927e01c40148
Andrei Iacob <77151602+Iacob-Andrei@users.noreply.github.com>
2023-03-18 11:00:04 +0200

    Infrastructure (#3)
    * discovery server added
    
    * api gateway
    
    * jib plugin
    
    * dockerized services
    
    * gitignore update
    
    * docker compose update

--- api-gateway/src/main/java/com/chefscorner/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/chefscorner/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.chefscorner.ingredient;
+package com.chefscorner.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class IngredientServiceApplication {
+public class ApiGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(IngredientServiceApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
