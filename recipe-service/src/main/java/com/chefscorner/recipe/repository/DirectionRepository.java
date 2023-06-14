fffa7e8fcfaa2b39ffac439d160b9527829d6b94
Iacob Andrei <andreiiacob15@gmail.com>
2023-06-14 16:05:32 +0300

    patch recipe

--- recipe-service/src/main/java/com/chefscorner/recipe/repository/DirectionRepository.java
+++ recipe-service/src/main/java/com/chefscorner/recipe/repository/DirectionRepository.java
@@ -13,9 +13,6 @@ import java.util.Optional;
 @Repository
 public interface DirectionRepository extends JpaRepository<Direction, Integer> {
 
-    @Query(value = "select d from Direction d join Recipe r on d.recipe = r and r.id = ?1")
-    List<Direction> findByRecipeId(Integer id);
-
     @Query(value = "select d from Direction d where d.step = ?1 and d.recipe.id = ?2")
     Optional<Direction> findByOrderAndRecipe(Integer order, Integer idRecipe);
 
